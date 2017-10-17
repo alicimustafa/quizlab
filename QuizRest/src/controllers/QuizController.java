211ee5fbee48a9e0c7f7320765abe5cfa1f1389b
mustafa alici <mustafaalici@mustafas-MacBook-Pro.local>
2017-10-17 16:51:03 -0600

    finnished quizcontroller and dao

--- QuizRest/src/controllers/QuizController.java
+++ QuizRest/src/controllers/QuizController.java
@@ -2,8 +2,11 @@ package controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +33,28 @@ public class QuizController {
 	@RequestMapping(path="quizes/{id}", method= RequestMethod.GET)
 	public Quiz show(@PathVariable int id) {
 		return dao.showById(id);
+	}
+	
+	@RequestMapping(path="quizes", method = RequestMethod.POST)
+	public Quiz create(@RequestBody String json, HttpServletResponse response) {
+		Quiz quiz = dao.create(json);
+		if(quiz != null) {
+			response.setStatus(201);
+		}
+		return quiz;
+	}
+	
+	@RequestMapping(path="quizes/{id}", method = RequestMethod.DELETE)
+	public String destroy(@PathVariable int id) {
+		if(dao.delete(id)) {
+			return "quiz deleted";
+		} else {
+			return "there was a problem";
+		}
+	}
+	
+	@RequestMapping(path="quizes/{id}", method = RequestMethod.PUT)
+	public Quiz update(@PathVariable int id, @RequestBody String json) {
+		return dao.update(id, json);
 	}
 }
--- QuizRest/src/data/QuizDAOImpl.java
+++ QuizRest/src/data/QuizDAOImpl.java
@@ -1,5 +1,6 @@
 package data;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -7,6 +8,10 @@ import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import entity.Quiz;
 
@@ -30,19 +35,53 @@ public class QuizDAOImpl implements QuizDAO {
 
 	@Override
 	public Quiz update(int id, String json) {
-		// TODO Auto-generated method stub
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			Quiz quizInput = mapper.readValue(json, Quiz.class);
+			Quiz quizData = em.find(Quiz.class, id);
+			quizData.setName(quizInput.getName());
+			return quizData;
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public Quiz create(String json) {
-		// TODO Auto-generated method stub
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			Quiz quiz = mapper.readValue(json, Quiz.class);
+			em.persist(quiz);
+			em.flush();
+			return quiz;
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public boolean delete(int id) {
-		// TODO Auto-generated method stub
+		Quiz quiz = em.find(Quiz.class, id);
+		if(quiz != null) {
+			em.remove(quiz);
+			return true;
+		}
 		return false;
 	}
 
